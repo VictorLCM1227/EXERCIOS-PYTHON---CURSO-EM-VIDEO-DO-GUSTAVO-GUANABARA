4b82f8b28ca418aaebd14c62f9ab291cd7145fe3
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-07-28 19:48:33 -0300

    Update 083 - Validando expressões matemáticas.py

--- Mundo-3/083 - Validando expressões matemáticas.py
+++ Mundo-3/083 - Validando expressões matemáticas.py
@@ -1,12 +1,15 @@
 expressao = input('Digite sua expressão: ')
-parentes_abertos = 0
-pareteses_fechados = 0
-for caractere in expressao:
-    if caractere == '(':
-        parentes_abertos += 1
-    elif caractere == ')':
-        pareteses_fechados += 1
-if parentes_abertos - pareteses_fechados == 0:
-    print('Sua expressão está certa!')
+pilha = []
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
 else:
-    print('A sua expressão está errada!')
+    print('Sua expressão está errada!')
