b371c91c29bee5306d1ff8c39a6da572c46a7299
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-07-28 19:31:11 -0300

    Update 083 - Validando expressões matemáticas.py

--- Mundo-3/083 - Validando expressões matemáticas.py
+++ Mundo-3/083 - Validando expressões matemáticas.py
@@ -1,1 +1,12 @@
 expressao = input('Digite sua expressão: ')
+parentes_abertos = 0
+pareteses_fechados = 0
+for caractere in expressao:
+    if caractere == '(':
+        parentes_abertos += 1
+    elif caractere == ')':
+        pareteses_fechados += 1
+if parentes_abertos - pareteses_fechados == 0:
+    print('Sua expressão está certa!')
+else:
+    print('A sua expressão está errada!')
