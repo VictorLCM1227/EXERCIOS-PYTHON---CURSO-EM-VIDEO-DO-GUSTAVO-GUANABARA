01c787298951115bfa92e10f613b1630e3cb3d6f
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-07-28 22:20:49 -0300

    Create 086 - Matriz em Python.py

---
+++ Mundo-3/086 - Matriz em Python.py
@@ -0,0 +1,10 @@
+matriz = [[[], [], []], [[], [], []], [[], [], []]]
+for linha in range(3):
+    for coluna in range(3):
+        matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
+print('-=' * 30)
+
+for linha in range(3):
+    for coluna in range(3):
+        print(f'[{matriz[linha][coluna]:^6}]', end='')
+    print()
