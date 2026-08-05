bfd67f6c2ced4b589578a6adcdf28f116515832d
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-05 15:23:38 -0300

    com correção

--- Mundo-3/098 - Função de Contador.py
+++ Mundo-3/098 - Função de Contador.py
@@ -1,10 +1,19 @@
+from time import sleep
+
 def contador(inicio, fim, passo):
     print('-=' * 30)
     print(f'Contagem de {inicio} até {fim} em {passo}')
+    sleep(2.5)
+    if passo == 0:
+        passo = 1
     if inicio > fim:
         passo *= -1
+        fim += -1
+    else:
+        fim += 1
     for valor in range(inicio, fim, passo):
-        print(valor, end=' ')
+        print(valor, end=' ', flush=True)
+        sleep(0.5)
     print('FIM!')
         
 contador(1, 10, 1)
