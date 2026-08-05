a8733a384e941a7d6b2d6bb7627eba9f3ce26f38
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-05 14:45:01 -0300

    sem ajuda

---
+++ Mundo-3/097 - Um print especial.py
@@ -0,0 +1,9 @@
+def escreva(texto):
+    tamanho = len(texto) + 4
+    print('~' * tamanho)
+    print(f'  {texto}')
+    print('~' * tamanho)
+
+escreva('Eu sou um programador')
+escreva('Python é muito legal')
+escreva('Frango Empanado')
