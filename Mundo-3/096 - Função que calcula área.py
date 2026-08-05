3ad9bb486959f683ec255789705bf50284a065e2
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-05 14:40:25 -0300

    depois de ver a esolução

--- Mundo-3/096 - Função que calcula área.py
+++ Mundo-3/096 - Função que calcula área.py
@@ -1,12 +1,11 @@
 
 def area(largura, comprimento):
     area_calculada = largura * comprimento
-    return area_calculada
+    print(f'A área de um terreno {largura:.1f}x{comprimento:.1f} é de {area_calculada:.1f}m²')
 
-
+#Programa principal
 print(' Controle de terrenos')
 print('-' * 30)
 largura = float(input('LARGURA (m): '))
 comprimento = float(input('COMPRIMENTO (m): '))
-area_calculda = area(largura, comprimento)
-print(f'A área de um terreno {largura:.1f}x{comprimento:.1f} é de {area_calculda:.1f}m²')
+area(largura, comprimento)
