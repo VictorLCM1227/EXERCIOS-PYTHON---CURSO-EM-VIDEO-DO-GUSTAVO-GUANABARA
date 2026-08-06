d00fdd1dc8da88577e9baa8e91b2badea926bf5d
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-06 13:23:03 -0300

    sem ajuda

--- Mundo-3/100 - Funções para sortear e somar.py
+++ Mundo-3/100 - Funções para sortear e somar.py
@@ -1,1 +1,29 @@
+from random import randint
+from time import sleep
+
 numeros = []
+
+def sorteia():
+    global numeros
+    print('Sorteando 5 valores da lista:', end=' ')
+    for numero in range(5):
+        while True:
+            numero = randint(0, 10)
+            if numero not in numeros:
+                break
+        print(numero, end=' ', flush=True)
+        sleep(0.2)
+        numeros.append(numero)
+    print('PRONTO!')
+
+
+def somaPar():
+    global numeros
+    soma = 0
+    for numero in numeros:
+        if numero % 2 == 0:
+            soma += numero
+    print(f'Somando os valores pares de {numeros} temos {soma}')
+
+sorteia()
+somaPar()
