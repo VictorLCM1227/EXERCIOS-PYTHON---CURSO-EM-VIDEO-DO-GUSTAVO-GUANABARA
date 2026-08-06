8f4adebff5a1008e5ddde8532766f02bbffe1459
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-06 13:54:38 -0300

    Update 101 - Funções para votação.py

--- Mundo-3/101 - Funções para votação.py
+++ Mundo-3/101 - Funções para votação.py
@@ -1,7 +1,15 @@
-from d
+
 
 def voto(ano_de_nascimento):
-
+    from datetime import date
+    ano_atual = date.today().year
+    idade = ano_atual - ano_de_nascimento
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade <= 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade}'
 
 
 print('-' * 30)
