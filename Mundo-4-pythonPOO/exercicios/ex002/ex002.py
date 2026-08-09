8b3be3c07061bda7837cbf34672498ff49cfdffe
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-09 14:25:17 -0300

    Update ex002.py

--- Mundo-4-pythonPOO/exercicios/ex002/ex002.py
+++ Mundo-4-pythonPOO/exercicios/ex002/ex002.py
@@ -2,10 +2,10 @@
 
 class Gafanhoto:
 
-    def __init__(self):  # Método Construtor
+    def __init__(self, n = '< DESCONHECIDO >', i = 0):  # Método Construtor
         # Atributos de Instância
-        self.nome = ""
-        self.idade = 0
+        self.nome = n
+        self.idade = i
 
     # Métodos de instância
     def aniversario(self):
@@ -17,14 +17,10 @@ class Gafanhoto:
 
 # Declaração de Objeto
 
-g1 = Gafanhoto()
-g1.nome = "Victor"
-g1.idade = 17
+g1 = Gafanhoto('Victor', 17)
 g1.aniversario()
 print(g1.mensagem())
 
-g2 = Gafanhoto()
-g2.nome = 'Sabrina'
-g2.idade = 17
+g2 = Gafanhoto('Sabrina', 17)
 g2.aniversario()
 print(g2.mensagem())
