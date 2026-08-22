c9837df10af063965881968bde9ef6cbc985619d
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-22 14:57:10 -0300

    não consegui

--- Mundo-4-pythonPOO/desafios/desafio017/desafio017.py
+++ Mundo-4-pythonPOO/desafios/desafio017/desafio017.py
@@ -1,3 +1,16 @@
-
+from rich import print
+from rich.panel import Panel
 
 class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    def etiqueta(self):
+        print(Panel((self.nome, self.preco), title='Produto', style='white', width=20))
+
+p1 = Produto('iPhone 17 Pro Max', 25_000.85)
+p2 = Produto('Notebook Gamer', 8_000)
+
+p1.etiqueta()
+p2.etiqueta()
