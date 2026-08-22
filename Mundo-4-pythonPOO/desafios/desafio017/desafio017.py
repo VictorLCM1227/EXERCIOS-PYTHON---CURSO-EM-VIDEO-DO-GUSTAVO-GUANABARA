85a3fbd68c9a9998e03c6f0023ca57f2642f237b
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-22 15:24:23 -0300

    com ajuda do professor

--- Mundo-4-pythonPOO/desafios/desafio017/desafio017.py
+++ Mundo-4-pythonPOO/desafios/desafio017/desafio017.py
@@ -7,7 +7,15 @@ class Produto:
         self.preco = preco
 
     def etiqueta(self):
-        print(Panel((self.nome, self.preco), title='Produto', style='white', width=20))
+        conteudo = f'{self.nome.center(30, " ")}'
+        conteudo += f'{"-" * 30}'
+        preco_formatado = f'R${self.preco:,.2f}'
+        conteudo += f'{preco_formatado.center(30, '.')}'
+        etiqueta = Panel(conteudo, title='Produto', width=34)
+        print(etiqueta)
+
+    def __str__(self):
+        return f'{self.nome} custa R${self.preco:,.2f}'
 
 p1 = Produto('iPhone 17 Pro Max', 25_000.85)
 p2 = Produto('Notebook Gamer', 8_000)
