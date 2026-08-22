f47bf58ac607618889186f4ca5e0251bcea9f08c
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-22 13:08:25 -0300

    novo exercicio

--- Mundo-4-pythonPOO/desafios/desafio016/desafio016.py
+++ Mundo-4-pythonPOO/desafios/desafio016/desafio016.py
@@ -1,17 +1,28 @@
 from rich import print
+from rich import inspect
 
 class Funcionario:
     def __init__(self, nome, setor, cargo):
+        #atributos de classe
+        empresa = "Curso em vídeo"
+
+        #Atributos de instância
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
-    def apresentacao(self):
-        return f':handshake: Olá, Sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa Curso em Vídeo' 
+    def apresentacao(self) -> str:
+        return f':handshake: Olá, Sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa {Funcionario.empresa}' 
 
+    
+
+Funcionario.empresa = 'Hostnet'
 
 c1 = Funcionario('Maria', 'Administração', 'Diretoria')
 print(c1.apresentacao())
+# inspect(c1, methods=True, dunder=True)
 
 c2 = Funcionario('Pedro', 'TI', 'Programador')
 print(c2.apresentacao())
+
+inspect(Funcionario)
