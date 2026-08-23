d2e9962655c7b566bcafa9890eefec52ce259e66
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-08-23 12:46:40 -0300

    com correção do professor

--- Mundo-4-pythonPOO/desafios/desafio18/desafio18.py
+++ Mundo-4-pythonPOO/desafios/desafio18/desafio18.py
@@ -3,26 +3,43 @@ from rich.panel import Panel
 
 
 class Churrasco:
+    # Atributos de Classe
+    consumo_padrao:float = 0.400
+    preco_kg:float = 82.40
+
+    # Método construtor
     def __init__(self, titulo, quantidade_participantes):
-        self.quantidade_participantes = quantidade_participantes
+        # Atributos de Instãncia
         self.titulo = titulo
+        self.quantidade_participantes = quantidade_participantes
+
+    def __str__(self):
+        return f'Esse é o {self.titulo} com {self.quantidade_participantes} pessoas participando.'
+
+    def calcular_quantidade_carne(self) ->float:
+        return self.quantidade_participantes * Churrasco.consumo_padrao
+
+    def calcular_custo_total(self) ->float:
+        return self.calcular_quantidade_carne() * self.__class__.preco_kg
+
+    def calcular_custo_individual(self) ->float:
+        return self.calcular_custo_total() / self.quantidade_participantes
+        
 
     def analisar(self):
-        preco = 82.40
-        carne_por_pessoa = 0.4
-        quantidade_carne = carne_por_pessoa * self.quantidade_participantes
-        custo_total = quantidade_carne * preco
-        preco_pessoa = preco * carne_por_pessoa
         conteudo = f'Analisando [green]{self.titulo}[/] com [blue]{self.quantidade_participantes} convidados[/]\n'
-        conteudo += f'Cada participante comerá {carne_por_pessoa}Kg e cada Kg custa R${preco:.2f}\n' 
-        conteudo += f'Recomendo [blue]comprar {quantidade_carne:.3f}kg[/] de carne\n'
-        conteudo += f'O custo total será de [green]R${custo_total:.2f}[/]\n'
-        conteudo += f'Cada pessoa pagará [yellow]R${preco_pessoa:.2f}[/] para participar.\n'
-        analise = Panel(conteudo, title=self.titulo, width=70)
-        print(analise)
+        conteudo += f'Cada participante comerá {Churrasco.consumo_padrao}Kg e cada Kg custa R${Churrasco.preco_kg:.2f}\n' 
+        conteudo += f'Recomendo [blue]comprar {self.calcular_quantidade_carne():.3f}kg[/] de carne\n'
+        conteudo += f'O custo total será de [green]R${self.calcular_custo_total():,.2f}[/]\n'
+        conteudo += f'Cada pessoa pagará [yellow]R${self.calcular_custo_individual():,.2f}[/] para participar.\n'
+        painel = Panel(conteudo, title=self.titulo)
+        print(painel)
 
 c1 = Churrasco('Churras dos Amigos', 15)
 c1.analisar()
+
+c2 = Churrasco('Festa do fim de ano', 80)
+c2.analisar()
 
 # 400g por pessoa
 # preco 82,40
