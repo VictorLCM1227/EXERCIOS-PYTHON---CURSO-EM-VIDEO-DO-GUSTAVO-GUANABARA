31949088e6de0ab135478f0f716c4a54db6f15b8
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-08-30 15:10:42 -0300

    vendo com o professor

--- Mundo-4-pythonPOO/desafios/desafio019/desfio19.py
+++ Mundo-4-pythonPOO/desafios/desafio019/desfio19.py
@@ -1,57 +1,31 @@
 from rich import print
-from rich.panel import Panel
+from time import sleep
 
 class Livro:
-
-    def __init__(self, titulo, paginas):
-        self.titilo = titulo
-        self.paginas = paginas
-        self.pagina_atual = 1
-        print(f":book: [blue]Você acabou de abrir o livro '[/][red]{self.titilo}[/][blue]' que tem [/][green]{self.paginas} paginas[/] [blue] no total. Você agora está na [/][yellow]página 1[/]")
-
-    def avancar_paginas(self, avanco):
-        avancos = 0
-        for pagina in range(avanco):
-            teste = self.pagina_atual
-            if teste == self.paginas:
-                print(f"[red]Você chegou ao final do livro '{self.titilo}'")
-                break
-            else:
-                print(f'Pag{pagina}', end=' ▶ ')
-                avancos += 1
-                self.pagina_atual += 1
-        print(f'[blue]Você avançou {avancos} páginas e agora está na [/][yellow]página {self.pagina_atual}[/]')
-
-
-l1 = Livro('10 coisas que aprendi', 20)
-l1.avancar_paginas(5)
-from rich import print
-from rich.panel import Panel
-
-class Livro:
-
     def __init__(self, titulo, paginas):
         self.titulo = titulo
-        self.paginas = paginas
+        self.total_paginas = paginas
         self.pagina_atual = 1
-        print(f":book: [blue]Você acabou de abrir o livro '[/][red]{self.titulo}[/][blue]' que tem [/][green]{self.paginas} paginas[/] [blue] no total. Você agora está na [/][yellow]página 1[/]")
 
-    def avancar_paginas(self, avanco):
-        avancos = 0
-        for pagina in range(avanco):
-            teste = self.pagina_atual
-            if teste == self.paginas:
-                print(f"[red]Você chegou ao final do livro '{self.titulo}'")
-                break
-            else:
-                print(f'Pag{pagina}', end=' ▶ ')
-                avancos += 1
+        print(f":open_book: [blue] Você acabou de abrir o livro [red]'{self.titulo}'[/red] que tem [green]{self.total_paginas} páginas[/] no total. Você agora está na [yellow]página {self.pagina_atual}[/yellow][/blue]")
+
+    def avancar_paginas(self, qtd = 1):
+        cont = 0
+        for pg in range(0, qtd, 1):
+            if not self.fim_do_livro():
                 self.pagina_atual += 1
-        self.pagina_atual += avancos
-        print(f'[blue]Você avançou {avancos} páginas e agora está na [/][yellow]página {self.pagina_atual}[/]')
+                print(f"Pág{self.pagina_atual} :arrow_forward: ", end='')
+                sleep(0.2)
+                cont += 1
+        print(f"[blue]Você avançou {cont} páginas e agora está na [yellow]página {self.pagina_atual}[/yellow][blue]")
+        if self.fim_do_livro():
+            print(f":closed_book: [red]Você chegou ao final do livro '{self.titulo}'[/red]")
 
+    def fim_do_livro(self):
+        return True if self.pagina_atual == self.total_paginas else False
 
 l1 = Livro('10 coisas que aprendi', 20)
 l1.avancar_paginas(5)
 l1.avancar_paginas(10)
-l1.avancar_paginas(100)
+l1.avancar_paginas(50)
+l1.avancar_paginas(5)
